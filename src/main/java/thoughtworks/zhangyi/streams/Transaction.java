0c0d09413f964e27823b0c1ae3ce259bbdbbbae6
zhangyi <bruce.e.zhang@gmail.com>
2015-01-05 21:56:31 +0800

    add example for streams

---
+++ src/main/java/thoughtworks/zhangyi/streams/Transaction.java
@@ -0,0 +1,16 @@
+package thoughtworks.zhangyi.streams;
+
+import java.util.Currency;
+
+public class Transaction {
+    private int price;
+    private Currency currency;
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+}
