fce2d382138fca8102efdf63687506190c62ae32
zhangyi <bruce.e.zhang@gmail.com>
2015-01-05 21:39:19 +0800

    add comments

--- src/main/java/thoughtworks/zhangyi/lambda/FileOperator.java
+++ src/main/java/thoughtworks/zhangyi/lambda/FileOperator.java
@@ -3,6 +3,11 @@ package thoughtworks.zhangyi.lambda;
 import java.io.File;
 import java.io.FileFilter;
 
+
+/**
+ * ::syntax means Java 8 method reference ("use this method as value")
+ * or using labmda: file -> file.isHidden()
+ */
 public class FileOperator {
     public File[] filterHiddenFiles() {
         return new File(".").listFiles(new FileFilter() {
