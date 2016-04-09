36119f5b6765adfa91eed8e28c8db5aca7b2f930
Zhang Yi <zhangyi@bigeyedata.com>
2016-04-09 20:27:09 +0800

    add demeter sample

---
+++ src/main/java/zhangyi/insight/oo/demeter/Customer.java
@@ -0,0 +1,16 @@
+package zhangyi.insight.oo.demeter;
+
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private Wallet myWallet;
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public Wallet getWallet(){
+        return myWallet;
+    }
+}
