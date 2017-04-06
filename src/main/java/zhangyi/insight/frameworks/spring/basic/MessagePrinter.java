b9f18cf2a68cc0bca0a08fd51ac5b362b3184a52
Zhang Yi <zhangyi@bigeyedata.com>
2017-04-06 17:07:11 +0800

    add hello spring sample

---
+++ src/main/java/zhangyi/insight/frameworks/spring/basic/MessagePrinter.java
@@ -0,0 +1,24 @@
+package zhangyi.insight.frameworks.spring.basic;/*                                                                      *\
+**                                                                      **
+**      __  __ _________ _____          ©Mort BI                        **
+**     |  \/  / () | () |_   _|         (c) 2015                        **
+**     |_|\/|_\____|_|\_\ |_|           http://www.bigeyedata.com       **
+**                                                                      **
+\*                                                                      */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessagePrinter {
+    final private MessageService messageService;
+
+    @Autowired
+    public MessagePrinter(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    public void printMessage() {
+        System.out.println(this.messageService.getMessage());
+    }
+}
