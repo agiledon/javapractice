669a31f95e6bb5fcc08c2972b2aabf1407985bd1
Zhang Yi <zhangyi@bigeyedata.com>
2017-04-06 13:57:40 +0800

    add the sample of thread pool

---
+++ src/test/java/zhangyi/insight/concurrency/threadpool/ThreadPoolSampleTest.java
@@ -0,0 +1,11 @@
+package zhangyi.insight.concurrency.threadpool;
+
+import org.junit.Test;
+
+public class ThreadPoolSampleTest {
+    @Test
+    public void should_execute_multi_tasks_in_thread_pool() {
+        ThreadPoolSample threadPoolSample = new ThreadPoolSample();
+        threadPoolSample.execute();
+    }
+}
